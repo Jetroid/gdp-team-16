fcec6cafb2175bd5dec8c6be9d1e7205d23dcb26
Jetroid <holtjet@gmail.com>
2017-10-24 10:30:07 +0100

    Add client side code

--- src/main/resources/static/js/annotations.js
+++ src/main/resources/static/js/annotations.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    var annotations = [];
+    var quotations = [];
     var selectionText = "";
     var selectionStartOffset = 0;
     var selectionEndOffset = 0;
@@ -7,19 +7,31 @@ window.onload = function() {
     // Rangy
     rangy.init();
     // highlighting
-    highlighter = rangy.createHighlighter(document, "TextRange");
-    highlighter.addClassApplier(rangy.createClassApplier("annotation", {
+    highlighter = rangy.createHighlighter(document.getElementById("case"), "TextRange");
+    highlighter.addClassApplier(rangy.createClassApplier("quotation", {
         ignoreWhiteSpace: true,
         elementTagName: "a",
         elementProperties: {
             onclick: function() {
-                annotationSelection(this);
+                quotationSelection(this);
             }
         }
     }));
 
-    // annotation link clicked (probably wont need this)
-    function annotationSelection(elem) {
+    $('#tabs a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show/Hide Tabs
+        $('#tab-container ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
+
+    // quotation link clicked (probably wont need this)
+    function quotationSelection(elem) {
         //var highlight = highlighter.getHighlightForElement(elem);
     }
 
@@ -52,8 +64,8 @@ window.onload = function() {
             selectionStartOffset = offsets.start;
             selectionEndOffset = offsets.end;
 
-            // check an identical annotation does not already exist
-            if (annotationExists(selectionStartOffset, selectionEndOffset)) {
+            // check an identical quotation does not already exist
+            if (quotationExists(selectionStartOffset, selectionEndOffset)) {
                 return;
             }
 
@@ -66,83 +78,167 @@ window.onload = function() {
                 })
                 .append($('<button type="button" id="create_annotation_button">Create Annotation</button>'))
                 .appendTo(document.body);
-            $('#create_annotation_button').click(createAnnotation);
+            $('#create_annotation_button').click(createQuotation);
         }
     }
 
-    function createAnnotation() {
+    function createQuotation() {
+        //TODO: CaseID hardcoded right now!
+        var caseId = 1;
         var text = selectionText;
         var start = selectionStartOffset;
         var end = selectionEndOffset;
 
-        // check an identical annotation does not already exist
-        if (annotationExists(start, end)) {
+        // check an identical quotation does not already exist
+        if (quotationExists(start, end)) {
             return;
         }
 
-        // create the annotation and push to array
-        // TODO: Unique ID's (involves backend)
-        var annotation = {start: start, end: end, text: text, id: 0};
-        annotations.push(annotation);
+        // create the quotation and push to array
+        var quotation = {
+            "caseId": caseId,
+            "startIndex": start,
+            "endIndex": end,
+            "quote": text,
+            "id": 0
+        };
+        quotations.push(quotation);
+
+        //Send our quotation to the server
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", '/putquotation', true);
+        //Send the proper header information along with the request
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.onreadystatechange = function() {//Call a function when the state changes.
+            if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                createAnnotation(parseInt(xhr.response), text);
+            }
+        }
+        xhr.send(JSON.stringify(quotation));
 
         // clear existing annotation buttons
         $('.create-annotation-ui').remove();
 
         // update styling
-        updateAnnotations();
+        updateStyling();
     }
 
-    // adds styling and links to all annotations
-    function updateAnnotations() {
+    function createAnnotation(quotationId, quoteText){
+        //TODO: authorId and caseId are hardcoded!
+        var authorId = 1;
+        var caseId = 1;
+
+        var annotationEditor = document.createElement("div");
+        annotationEditor.className = "annotation-container";
+        annotationEditor.id = "annotation-editor";
+
+        var titleElem = document.createElement("h2");
+        titleElem.innerHTML = "Create Annotation";
+        annotationEditor.appendChild(titleElem);
+
+        var quotationContainer = document.createElement("div");
+        annotationEditor.appendChild(quotationContainer);
+        quotationContainer.className = "quotation-container";
+
+        var quotation = document.createElement("cite");
+        quotationContainer.appendChild(quotation);
+        quotation.className = "quotation";
+        quotation.innerHTML = quoteText;
+
+        var textArea = document.createElement("textArea");
+        textArea.id = "annotationEdit";
+        annotationEditor.appendChild(textArea);
+
+        var submitButton = document.createElement("button");
+        submitButton.innerHTML = "Submit Annotation";
+        annotationEditor.appendChild(submitButton);
+        submitButton.onclick = function(){
+            var annotation = {
+                "caseId": caseId,
+                "authorId": authorId,
+                "quotationId": quotationId,
+                "text": textArea.value,
+                "id": 0
+            };
+
+            //Send our annotation to the server
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", '/putannotation', true);
+            //Send the proper header information along with the request
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onreadystatechange = function() {//Call a function when the state changes.
+                if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                    convertEditorToAnnotation(xhr.response, textArea.value);
+                }
+            }
+            xhr.send(JSON.stringify(annotation));
+        };
+
+        $('#annotations').prepend(annotationEditor);
+
+        // Show/Hide Tabs
+        $('#tab-container #annotations-container').show().siblings().hide();
+        // Change/remove current tab to active
+        $('#tabs #annotations-container').parent('li').addClass('active').siblings().removeClass('active');
+
+    }
+
+    function convertEditorToAnnotation(annotationId, annotationText){
+        //TODO: Author name hardcoded!
+        var authorName = "Tom Jones";
+        var editor = document.getElementById("annotation-editor");
+        editor.id = "annotation" + annotationId;
+        editor.firstChild.innerHTML = "Written by " + authorName;
+        //Remove button
+        editor.removeChild(editor.lastChild);
+        //Remove textarea
+        editor.removeChild(editor.lastChild);
+
+        var p = document.createElement("p");
+        p.innerHTML = annotationText;
+        editor.appendChild(p);
+    }
+
+    // adds styling and links to all quotations
+    function updateStyling() {
         // clear existing highlights
         highlighter.removeAllHighlights();
 
-        /* *old method*
-            // create a new range encompassing the annotation text and apply it to the selection
-            var selection = rangy.getSelection();
+        // convert all quotations into CharacterRange objects
+        for (var i = 0; i < quotations.length; i++) {
+            var quo = quotations[i];
+            // create a new range encompassing the quotation text and apply it to the selection
             var range = rangy.createRange();
-            range.selectCharacters(document.getElementById('case'), ann.start, ann.end);
-            selection.setSingleRange(range);
-
-            // apply the "annotation" class
-            highlighter.highlightSelection("annotation", {exclusive: true, containerElementId: "case"});
-        */
-
-        // convert all annotations into CharacterRange objects
-        for (var i = 0; i < annotations.length; i++) {
-            var ann = annotations[i];
-            // create a new range encompassing the annotation text and apply it to the selection
-            var range = rangy.createRange();
-            range.selectCharacters(document.getElementById('case'), ann.start, ann.end);
+            range.selectCharacters(document.getElementById('case'), quo.start, quo.end);
 
             // from the range we can convert to a CharacterRange
             var converter = highlighter.converter;
             var charRange =  converter.rangeToCharacterRange(range, document.getElementById('case'));
 
-            // each annotation is applied one at a time so the correct annotation link can be manually inserted
+            // each quotation is applied one at a time so the correct quotation link can be manually inserted
             var charRanges = [];
             charRanges.push(charRange);
-            highlighter.highlightCharacterRanges("annotation", charRanges);
+            highlighter.highlightCharacterRanges("quotation", charRanges);
 
             // add links manually to the highlights
-            var highlights = document.getElementsByClassName("annotation");
+            var highlights = document.getElementsByClassName("quotation");
             for(var j = 0; j < highlights.length; j++) {
-                // so annotations with links aren't overwritten
+                // so quotations with links aren't overwritten
                 if(highlights[j].href !== "") {
                     continue;
                 }
-                // set the link to be the annotations discussion page with an anchor pointing to the annotation
-                highlights[j].href = "annotations#" + ann.start + "-" + ann.end;
+                // set the link to be the quotations discussion page with an anchor pointing to the quotation
+                highlights[j].href = "quotations#" + quo.start + "-" + quo.end;
             }
         }
     }
 
-    // returns true if there exists an annotation with the identical start and end indexes
-    function annotationExists(start, end) {
-        // check an identical annotation does not already exist
-        for (var i = 0; i < annotations.length; i++) {
-            // annotation already exists
-            if (annotations[i].start === start && annotations[i].end === end) {
+    // returns true if there exists an quotation with the identical start and end indexes
+    function quotationExists(start, end) {
+        // check an identical quotation does not already exist
+        for (var i = 0; i < quotations.length; i++) {
+            // quotation already exists
+            if (quotations[i].start === start && quotations[i].end === end) {
                 return true;
             }
         }
